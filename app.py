d807755f5d9178603f3755362022fbffc3c40249
Ivy Zhou <ivyzhou0512@outlook.com>
2024-04-16 18:49:30 -0500

    streamlit para parsing test

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ from utils.explore_page import show_explore_page
 from utils.model_page import compare_model_page
 from utils.predict_page import show_predict_page
 
+import os
+import sys
+import random
+import argparse
+
+
+parser = argparse.ArgumentParser(description='pull in 3 parameters: feature, target, and models')
+
+parser.add_argument('features', type = str, require = True, help = "github path to the feature category")
+parser.add_argument('target', type = str, require = True, help = "target dataset")
+parser.add_argument('models', type = str, require = True, help = "maching leanring model type")
+args = parser.parse_args()
+
 
 page = st.sidebar.selectbox("Explore Or Predict Or Else", ("Understanding the Data","Compare Models","Predict"))
 
