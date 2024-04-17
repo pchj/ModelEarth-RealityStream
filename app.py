ce01c467d873a28fad5deb334e9bc0ac988e3a99
Ivy Zhou <98992056+Ultracatx@users.noreply.github.com>
2024-04-16 20:51:21 -0500

    testing deployment on streamlit without changing para

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from utils.explore_page import show_explore_page
 from utils.model_page import compare_model_page
 from utils.predict_page import show_predict_page
 
+'''
 import os
 import sys
 import random
@@ -16,7 +17,7 @@ parser.add_argument('target', type = str, require = True, help = "target dataset
 parser.add_argument('models', type = str, require = True, help = "maching leanring model type")
 args = parser.parse_args()
 
-
+'''
 page = st.sidebar.selectbox("Explore Or Predict Or Else", ("Understanding the Data","Compare Models","Predict"))
 
 if page == "Understanding the Data":
